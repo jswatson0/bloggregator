55c682cbcb0a64bcb4fc5478607f18cbe14892b2
Jason Watson <jswatson0@gmail.com>
2013-07-28 16:51:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggregator::Application.config.secret_token = '2540976bedaf53cb451dfa6a7ce7f139b8c88c2577c5b2edfa3ff00f15779019ecb7fd0dfe785916330129f7774f6eb00dbf022158bb590a0ad552cf30729bf1'
