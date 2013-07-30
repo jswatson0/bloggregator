7e1a8b4405ed45294d8097a5ab27f165fedefeb8
Jason Watson <jswatson0@gmail.com>
2013-07-30 16:49:56 -0500

    Add font

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
+  
 end
