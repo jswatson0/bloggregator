467cab9e6e107a1dc2e94b6a6cda4c36f4e13aae
Jason Watson <jswatson0@gmail.com>
2013-08-11 19:03:12 -0500

    added js

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -4,16 +4,16 @@ class FeedEntry < ActiveRecord::Base
   def self.update_from_feed(feed_url)
   	feed = Feedzirra::Feed.fetch_and_parse(feed_url)
     @entry = feed.entries
-  	# feed.entries.each do |entry|
-  	# 	unless exists? :guid => entry.id
-  	# 		create!(
-  	# 			:title        => entry.title,
-  	# 			:summary      => entry.summary,
-  	# 			:url          => entry.url,
-  	# 			:published_at => entry.published,
-  	# 			:guid         => entry.id
-  	# 		)
-  	# 	end
-  	# end
+  	feed.entries.each do |entry|
+  		unless exists? :guid => entry.id
+  			create!(
+  				:title        => entry.title,
+  				:summary      => entry.summary,
+  				:url          => entry.url,
+  				:published_at => entry.published,
+  				:guid         => entry.id
+  			)
+  		end
+  	end
   end
 end
